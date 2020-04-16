1f3531cd621c041bbd85caddddcf04fe7f6f1728
paqu <pawel.kusz1993@gmail.com>
2020-04-16 23:20:55 +0200

    Create base server for further work

---
+++ src/main/java/com/paqu/rest/Main.java
@@ -0,0 +1,28 @@
+package com.paqu.rest;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class Main {
+    public static final int PORT = 1234;
+    public static final String BASE_URI = "http://localhost:"+ PORT + "/";
+
+    public static HttpServer startServer() {
+
+        // JAX-RS resources and providers
+        final ResourceConfig rc = new ResourceConfig().packages("com.paqu.rest");
+
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+
+    public static void main(String[] args) throws IOException {
+        final HttpServer server = startServer();
+        System.out.println("Server listening on " + PORT + "\nHit enter to stop it...");
+        System.in.read();
+        server.stop();
+    }
+}
