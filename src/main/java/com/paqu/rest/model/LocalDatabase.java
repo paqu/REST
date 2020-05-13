b6c31bc1d0f622e17ef1a626e57530e4f638e4f5
paqu <pawel.kusz1993@gmail.com>
2020-05-13 05:20:56 +0200

    Add mongodb as database in project & getStudents from mongo

--- src/main/java/com/paqu/rest/model/LocalDatabase.java
+++ src/main/java/com/paqu/rest/model/LocalDatabase.java
@@ -1,11 +1,52 @@
 package com.paqu.rest.model;
 
+import com.mongodb.MongoClient;
+import dev.morphia.Datastore;
+import dev.morphia.Morphia;
+
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class LocalDatabase {
+
+    private final MongoClient client;
+    private final Morphia morphia;
+    static private Datastore database;
+
     static private LocalDatabase instance = new LocalDatabase();
+
+
+
+    //public Datastore getDatabase() { return database; }
+/*
+    static {
+        initDB(new LocalDatabase());
+    }
+*/
+    private LocalDatabase() {
+        this.client = new MongoClient("localhost", 8004);
+        this.morphia = new Morphia();
+        this.morphia.getMapper().getOptions().setStoreEmpties(true);
+        this.morphia.mapPackage("model");
+        this.database = this.morphia.createDatastore(client, "SIDBStudents");
+        this.database.ensureIndexes();
+    }
+/*
+    public static void initDB(LocalDatabase model) {
+        Datastore db = model.getDatabase();
+
+        if (db.getCount(Student.class) > 0 || db.getCount(Course.class) > 0) {
+            return;
+        }
+        Student s1 = new Student(109787, "Pawel", "Kusz", new Date(93, 10, 17), new ArrayList<Grade>());
+        Student s2 = new Student(117123, "Piotr", "Kusz", new Date(93, 10, 17), new ArrayList<Grade>());
+        Student s3 = new Student(123456, "Romek", "Kaczynski", new Date(65, 9, 23), new ArrayList<Grade>());
+        Student s4 = new Student(100000, "Jarek", "Giertych", new Date(45, 10, 1), new ArrayList<Grade>());
+
+    }
+  */
+
     static private final Map<Integer, Student> StudentDB = new ConcurrentHashMap<Integer, Student>();
     static private final Map<Integer, Course> CourseDB = new ConcurrentHashMap<Integer, Course>();
     static private AtomicInteger indexCounter;
@@ -14,14 +55,16 @@ public class LocalDatabase {
 
 
     static {
-        indexCounter  = new AtomicInteger(15000);
+        indexCounter  = new AtomicInteger(109790);
         courseIdCounter = new AtomicInteger( 2);
         gradeIdCounter = new AtomicInteger(7);
 
+
         Student s1 = new Student(109787, "Pawel", "Kusz", new Date(93, 10, 17), new ArrayList<Grade>());
-        Student s2 = new Student(117123, "Piotr", "Kusz", new Date(93, 10, 17), new ArrayList<Grade>());
-        Student s3 = new Student(123456, "Romek", "Kaczynski", new Date(65, 9, 23), new ArrayList<Grade>());
-        Student s4 = new Student(100000, "Jarek", "Giertych", new Date(45, 10, 1), new ArrayList<Grade>());
+        Student s2 = new Student(109788, "Piotr", "Kusz", new Date(93, 10, 17), new ArrayList<Grade>());
+        Student s3 = new Student(109789, "Romek", "Kaczynski", new Date(65, 9, 23), new ArrayList<Grade>());
+        Student s4 = new Student(109790, "Jarek", "Giertych", new Date(45, 10, 1), new ArrayList<Grade>());
+
 
         Course c1 = new Course (1,"Technologie Programistyczne - Systemy internetowe", "Tomasz Pawlak");
         Course c2 = new Course (2,"Biznesowe Systemy Rozproszone", "Tomasz Pawlak");
@@ -38,12 +81,38 @@ public class LocalDatabase {
         s3.getGrades().add(new Grade(7, 2.0f, new Date(), c2));
 
         StudentDB.put(109787, s1);
-        StudentDB.put(117123, s2);
-        StudentDB.put(123456, s3);
-        StudentDB.put(100000, s4);
+        StudentDB.put(109788, s2);
+        StudentDB.put(109789, s3);
+        StudentDB.put(109790, s4);
 
         CourseDB.put(1, c1);
         CourseDB.put(2, c2);
+
+        if (database.getCount(Student.class)         == 0
+                && database.getCount(Course.class)   == 0
+                && database.getCount(Index.class)    == 0
+                && database.getCount(GradeId.class)  == 0
+                && database.getCount(CourseId.class) == 0)
+        {
+
+            database.save(c1);
+            database.save(c2);
+            CourseId courseId = new CourseId(2);
+            database.save(courseId);
+
+            database.save(s1);
+            database.save(s2);
+            database.save(s3);
+            database.save(s4);
+
+            Index index = new Index(109790);
+            database.save(index);
+
+            GradeId gradeId = new GradeId(7);
+            database.save(gradeId);
+
+            System.out.println("saved");
+        }
 
         System.out.println("Local database initialized");
     }
@@ -52,19 +121,44 @@ public class LocalDatabase {
         return instance;
     }
 
+    public long getNextIndex() {
+        Index index = database.findAndModify(database.createQuery(Index.class),
+                database.createUpdateOperations(Index.class).inc("lastIndex", 1));
+        if (index == null) {
+            return -1;
+        }
+        return index.getLastIndex();
+    }
+
     public int addStudent(Student student) {
         int index = indexCounter.incrementAndGet();
         student.setIndex(index);
         StudentDB.put(index, student);
         return index;
     }
+
     public Student getStudent(int index) {
         return StudentDB.get(index);
     }
+/*
+    public Student getStudent(long index) {
+        return database.createQuery(Student.class).filter("index", index).first();
+
+    }
+*/
+/*
     public Collection<Student> getStudents() {
         List<Student> studentList = new ArrayList<Student>(StudentDB.values());
         return studentList;
     }
+    */
+
+    public Collection<Student> getStudents() {
+        List<Student> studentList = database.createQuery(Student.class).find().toList();
+        return studentList;
+    }
+
+
     public Student updateStudent(int index, Student update)
     {
         Student current = StudentDB.get(index);
@@ -86,6 +180,18 @@ public class LocalDatabase {
     };
 
 
+
+
+
+
+    public long getNextCourseId() {
+        CourseId courseId = database.findAndModify(database.createQuery(CourseId.class),
+                database.createUpdateOperations(CourseId.class).inc("lastCourseId", 1));
+        if (courseId == null) {
+            return -1;
+        }
+        return courseId.getLastCourseId();
+    }
 
     public int addCourse(Course course)
     {
@@ -120,6 +226,15 @@ public class LocalDatabase {
     {
         List<Course> studentList = new ArrayList<Course>(CourseDB.values());
         return studentList;
+    }
+
+    public long getNextGradeId() {
+        GradeId gradeId = database.findAndModify(database.createQuery(GradeId.class),
+                database.createUpdateOperations(GradeId.class).inc("lastGradeId", 1));
+        if (gradeId == null) {
+            return -1;
+        }
+        return gradeId.getLastGradeId();
     }
 
     public int addGrade(int index, Grade grade)
